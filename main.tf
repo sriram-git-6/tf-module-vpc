615ae0c5cc8f537957de42a8efb5cf371f093178
sriram <sriram.mudunuru@gmail.com>
2023-09-04 18:36:11 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 }
-resource "aws_subnet" "main" {
-  for_each = var.subnets
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = ""
 
-  }
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+  subnets = each.value
+  vpc_id = aws_vpc.main.id
+}
